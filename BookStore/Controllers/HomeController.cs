11d4b81d59b196ffbab01f38cbe75c308c992c34
showrav-BS1334 <showrav.paul@brainstation-23.com>
2023-04-15 23:17:08 +0600

    Add project files.

---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
