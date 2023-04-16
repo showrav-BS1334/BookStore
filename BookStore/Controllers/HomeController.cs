c7fc55b98d207fabfb228c9d6906a3f9ecf977ac
showrav-BS1334 <showrav.paul@brainstation-23.com>
2023-04-16 11:52:43 +0600

    book details page done, update and delete buttons added

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -14,14 +14,81 @@ namespace BookStore.Controllers
             _db = db;
         }
 
-        // action for home page ----------------------------------------------------------
+        // action for home page ------------------------------------------------------------
         public IActionResult Index()
         {
             IEnumerable<Book> objBook = _db.Books; // books table ta fetch kore anlam
             return View(objBook);
         }
 
-        
+        // for showing book details ----------------------------------------------------------
+        public IActionResult ShowBookDetails(int? id)
+        {
+            var book = _db.Books.Find(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            var author = _db.Authors.Find(book.AuthorId);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            ViewBag.author = author;
+            ViewBag.book = book;
+            return View();
+        }
+
+        // for updating book details --------------------------------------------------------
+        // GET
+        public IActionResult UpdateBook(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var book = _db.Books.Find(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            var author = _db.Authors.Find(book.AuthorId);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            ViewBag.author = author;
+            ViewBag.book = book;
+            return View();
+        }
+
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateBook(Book obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Books.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // for removing a book ---------------------------------------------------------
+        public IActionResult DeleteBook(int? id)
+        {
+            var obj = _db.Books.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Books.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
